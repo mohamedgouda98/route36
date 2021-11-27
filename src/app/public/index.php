eb0cb5076601b90c148aca8f5ed2bd3459448cac
macbookpro <dev.mohamedgouda@gmail.com>
2021-11-27 16:29:35 +0200

    init git

---
+++ src/app/public/index.php
@@ -0,0 +1,185 @@
+<?php
+//$number1 = 10;
+//$number2 = 20;
+//$number3 = 30;
+//
+//$avg = ($number1 + $number2 + $number3) / 3;
+//
+//echo $avg;
+
+//$salary = 10000;
+//$tax = 0.1;
+//
+//$subTotal = $salary * $tax;
+//
+//$netSalary = $salary - $subTotal;
+//
+//echo $netSalary;
+
+//
+//$hours = 100;
+//$hourRate1 = 100;
+//$hourRate2 = 80;
+//$tax = 0.1;
+//
+//if($hours > 0 )
+//{
+//    if($hours >= 100)
+//    {
+//        $subTotal = $hours * $hourRate1;
+//    }else{
+//        $subTotal = $hours * $hourRate2;
+//    }
+//
+//    $totalTax = $tax * $subTotal;
+//    $netSalary = $subTotal - $totalTax;
+//    echo $netSalary;
+//}
+//else{
+//    echo "wrong data";
+//}
+//
+//
+//$year = 1901;
+//if($year % 4 == 0)
+//{
+//    echo "29";
+//}else
+//{
+//    echo "28";
+//}
+//
+
+
+//$number = 10;
+//
+//$numbers = [1,2,3,4,5];
+//
+//print_r($numbers);
+//echo '<br>';
+//
+//$numbers[100] = 100;
+//$numbers[] = 200;
+//
+//print_r($numbers);
+//
+//
+
+//
+//for($i=0; $i <= 20; $i++)
+//{
+//    echo $i . '<br>';
+//}
+//
+
+//for($i=0; $i<=20; $i+=2)
+//{
+//    echo $i . '<br>';
+//}
+
+//$numbers  = [0,1,2,3,4,5,6,7,8,9,10];
+//
+//
+//for($i=0 ; $i <= count($numbers)-1 ; $i++)
+//{
+//    echo $numbers[$i] . '<br>';
+//}
+
+
+//$numbers = [4,15,3];
+//
+//$sum = 0;
+//
+//for($i=0; $i <= count($numbers)-1; $i++)
+//{
+//    $sum = $sum + $numbers[$i];
+//}
+//
+//echo $sum / count($numbers);
+
+//
+//$numbers = [10,20,3,7,8];
+//$evenNumbers = [];
+//$oddNumbers = [];
+//
+//for($i=0; $i<= count($numbers)-1; $i++)
+//{
+//    if($numbers[$i] % 2 == 0)
+//    {
+//        $evenNumbers[] = $numbers[$i];
+//    }else
+//    {
+//        $oddNumbers[] = $numbers[$i];
+//    }
+//}
+//
+//print_r($evenNumbers);
+//echo '<br> <hr><br>';
+//print_r($oddNumbers);
+
+
+
+//$names = ['mohamed', 'ahmed', 'ali'];
+//$name = "ali";
+//
+//for($i=0; $i<= count($names)-1; $i++)
+//{
+//   if($name == $names[$i])
+//   {
+//       echo "exists<br>";
+//       break;
+//   }else
+//   {
+//       echo 'not found<br>';
+//   }
+//}
+
+
+//$names = ['mohamed', 'ahmed', 'ali'];
+//$name = "gouda";
+//$isExists = false;
+//
+//for($i=0; $i<= count($names)-1; $i++)
+//{
+//    if($name == $names[$i])
+//    {
+//        echo 'exists<br>';
+//        $isExists = true;
+//        break;
+//    }
+//}
+//
+//
+//if($isExists == false)
+//{
+//    $names[] = $name;
+//}
+
+
+//print_r($names);
+
+
+//$subjects = [30,50,80,9,20];
+//$sum=array_sum($subjects);
+//$passDeg = (count($subjects) * 100) / 2;
+//
+//if($sum >= $passDeg)
+//{
+//    echo "true";
+//}else
+//{
+//    echo "false";
+//}
+
+//$names = ['mohamed', 'ahmed', 'ali'];
+//$name = "mohamed";
+
+// if(in_array($name, $names))
+// {
+//     echo "yes";
+// }else
+// {
+//     echo "no";
+// }
+
+
